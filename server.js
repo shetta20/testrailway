f30409bf00bad19cc13e3dfeccd524c4bace1394
shetta20 <ashish94shetty@gmail.com>
2022-11-24 09:19:18 +0530

    test

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/profile', (req,res)=> {
+const user = {
+name:'sally',
+hobby:'soccer'
+}
+res.send(user);
+})
+
+
+app.get('/', (req,res)=> {
+res.send("Getting root");
+})
+
+app.listen(3000);
